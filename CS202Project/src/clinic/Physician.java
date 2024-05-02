f4de34f1a7f9672212e2ea6e193c104a16ee171c
nhuntera <nhuntera2112@gmail.com>
2024-05-02 15:08:38 -0500

    Added methods to add and list appointments

--- CS202Project/src/clinic/Physician.java
+++ CS202Project/src/clinic/Physician.java
@@ -1,14 +1,19 @@
 package clinic;
 
+import java.util.ArrayList;
+
 public class Physician {
 	private String name;
 	// array of hours available in 24 time format
 	private int[] avalableHours;
-	
+	private ArrayList<String> appointments;
 //Constructor
 	public void physician(String name, int[] avalableHours) {
 		this.name = name;
 		this.avalableHours = avalableHours;
+		
+		this.appointments = new ArrayList<String>();
+		
 	}
 	
 //Getters
@@ -28,6 +33,18 @@ public class Physician {
 			}
 		}
 		return false;
+		
+	}
+	
+	public void addAppointment(String appointment) {
+		
+		appointments.add(appointment);
+		
+	}
+	
+	public void ListAppointments() {
+		
+		System.out.println(appointments.toString());
 		
 	}
 
